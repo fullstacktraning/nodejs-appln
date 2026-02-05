96724a833f953bcbc9ecefce58c01018db41f625
CareerIT <samba.pendela@gmail.com>
2026-02-05 13:27:36 +0530

    registration completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,8 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
 exports.register = async (req,res)=>{
     try{
         const {name,email,password} = req.body;
@@ -43,5 +44,64 @@ exports.login = async (req,res)=>{
 
     }catch(err){
         return res.status(500).json({"message":"server side error"});
+    }
+}
+
+exports.forgetPassword = async (req,res)=>{
+    try{
+        const {email} = req.body;
+        const db_user = await User.findOne({email});
+        if(!db_user){
+            return res.status(400).json({"message":"User not found"});
+        }
+
+        token = crypto.randomBytes(32).toString("hex");
+        db_user.resetToken = token;
+        db_user.resetTokenExpire = Date.now() + 10*60*1000;
+        db_user.save();
+
+        const transporter = nodemailer.createTransport({
+            service:"gmail",
+            auth:{
+                user:process.env.EMAIL,
+                pass:process.env.PASSWORD
+            }
+        });
+
+        const resetLink = `http://localhost:8000/api/auth/reset-password/${token}`;
+
+        await transporter.sendMail({
+            to:db_user.email,
+            subject:"Password Reset Link",
+            html:`<p>Click Below Link to Reset Password :</p>
+                 <a href="${resetLink}">${resetLink}</a>`
+        });
+        res.status(200).json({"message":"Reset Link Sent Successfully !!!"});
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({"message":"Server Side Error"});
+    }
+}
+
+exports.resetPassword = async (req,res)=>{
+    try{
+        const {password} = req.body;
+        const {token} = req.params;
+        const db_user = await User.findOne({
+            resetToken:token,
+            resetTokenExpire:{$gt:Date.now()}
+        })
+        if(!db_user){
+            return res.status(400).json({"message":"Token Expired and Try Again"})
+        }
+        const hashedPassword = await bcrypt.hash(password,10);
+        db_user.password = hashedPassword;
+        db_user.resetToken = undefined;
+        db_user.resetTokenExpire = undefined;
+        db_user.save();
+        res.status(200).json({"message":"password reset successfully !!!"});
+    }catch(err){
+        return res.status(500).json({"message":"Internal Server Error"});
     }
 }
